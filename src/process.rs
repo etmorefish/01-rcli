28bd1d823519ec8dbd2680b75a36ff7aed537fd2
maolei <maolei025@qq.com>
2024-11-23 23:42:34 +0800

    fix: csv cli support any json

--- src/process.rs
+++ src/process.rs
@@ -3,6 +3,7 @@ use std::fs;
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -21,11 +22,17 @@ pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
 
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        // println!("{:?}", record);
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        // headers.iter() -> 使用 headers 的迭代器
+        // record.iter() -> 使用 record 的迭代器
+        // zip() -> 将两个迭代器合并为一个元组的迭代器 [(header, record), ..]
+        // collect::<Value>() -> 将元组的迭代器转换为 JSON Value
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
+
     let json = serde_json::to_string_pretty(&ret)?;
     fs::write(output, json)?;
 
