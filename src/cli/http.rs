fc8c130b749ca0eaffdd6627dab5fed8b363833b
maol <maol@nanofilm.com.cn>
2024-11-29 16:13:54 +0800

    feat: http static serve

---
+++ src/cli/http.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+use super::verify_path;
+
+#[derive(Debug, Parser)]
+pub enum HttpSubCommand {
+    #[command(about = "Serve a directory over HTTP")]
+    Serve(HttpServeOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct HttpServeOpts {
+    /// The directory to serve
+    #[arg(short, long, value_parser = verify_path, default_value = ".")]
+    pub dir: PathBuf,
+    /// The port to serve on
+    #[arg(short, long, default_value_t = 8080)]
+    pub port: u16,
+}
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,12 @@
 mod base64;
 mod csv;
 mod genpass;
+mod http;
 
 use clap::Parser;
 
-pub use self::{base64::*, csv::*, genpass::*};
-use std::path::Path;
+pub use self::{base64::*, csv::*, genpass::*, http::*};
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Parser)]
 #[clap(name = "rcli", author, version, about, long_about = None)]
@@ -24,6 +25,9 @@ pub enum SubCommand {
 
     #[command(subcommand)]
     Base64(Base64SubCommand),
+
+    #[command(subcommand)]
+    Http(HttpSubCommand),
 }
 
 pub fn verify_input_file(filename: &str) -> Result<String, &'static str> {
@@ -48,5 +52,14 @@ pub fn parse_base64_format(format: &str) -> Result<Base64Format, &'static str> {
         "standard" => Ok(Base64Format::Standard),
         "urlsafe" => Ok(Base64Format::UrlSafe),
         _ => Err("Invalid format"),
+    }
+}
+
+pub fn verify_path(path: &str) -> Result<PathBuf, &'static str> {
+    let path = PathBuf::from(path);
+    if path.exists() && path.is_dir() {
+        Ok(path.into())
+    } else {
+        Err("Path does not exist or is not a directory")
     }
 }
